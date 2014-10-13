ba62e1495273ebb94e69592230a9f311c0fd31aa
Teodor Heggelund <teodor.heggelund@gmail.com>
2014-10-13 23:30:35 +0200

    Fun with do-notation

--- source/do.hs
+++ source/do.hs
@@ -1,6 +1,42 @@
-import types
+import Types
+import Control.Monad (guard, MonadPlus, mzero, mplus)
 
 main = do
 	name <- getLine
 	putStrLn $ "Hello, " ++ name ++ "!"
 
+grandfathers'' p = do
+	ff <- father p >>= father
+	mf <- mother p >>= father
+	return (ff, mf)
+
+grandfathersFail p = do
+	ff <- father p >>= father
+	mf <- mother p >>= father
+	_ <- Tull
+	return (ff, mf)
+
+pythagoreans = do
+	z <- [1..]
+	y <- [1..z]
+	x <- [1..y]
+	guard (x*x + y*y == z*z)
+	return (x, y, z)
+
+pythagoreans' = do
+	z <- [1..]
+	y <- [1..z]
+	x <- [1..y]
+	_ <- filter (\w -> x*x + y*y == z*z) [1]
+	return (x, y, z)
+
+instance MonadPlus Kanskje where
+	mzero = Tull
+	mplus (Bare x) _  = Bare x
+	mplus _        kx = kx
+
+grandfathersNotHitler p = do
+	ff <- father p >>= father
+	mf <- mother p >>= father
+	guard (name ff /= "Hitler")
+	return (ff, mf)
